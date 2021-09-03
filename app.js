b3381e13a2603d1fbfefb601ca25d2e355d93bf6
jigmenima <jigmenimalhamo@gmail.com>
2021-09-03 13:02:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const ip = "127.0.0.1"
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    //console.log(req);
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from server!!!');
+})
+
+server.listen(port, ip, () => console.log('Server running...'));
+
+
